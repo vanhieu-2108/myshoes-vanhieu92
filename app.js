5837a02b74bbc22522cca742f9e93d587f07674a
levanhieusona <levanhieusona@gmail.com>
2023-04-06 22:48:59 +0700

    Fix Iframe

--- app.js
+++ app.js
@@ -24,4 +24,45 @@ window.addEventListener("load", () => {
         modalSignUp.classList.add("active");
         modalSignUp.style = `animation: fadeIn 0.5s forwards linear`;
     });
+    const bag = document.querySelector(".bag");
+    const pBag = document.querySelector("p.bag");
+    let bagIndex = 0;
+    if (pBag) {
+        pBag.addEventListener("click", function (e) {
+            bagIndex++;
+            bag.style = `--text--: "${bagIndex}"`;
+        });
+    }
+    const formSignIn = document.querySelector(".form-sign-in");
+    const usernamePSignIn = document.querySelector(".username-sign-in");
+    const passworldPSignIn = document.querySelector(".passworld-sign-in");
+    formSignIn.addEventListener("submit", function (e) {
+        e.preventDefault();
+        let isValid = true;
+        if (formSignIn[0].value.trim() === "") {
+            isValid = false;
+            formSignIn[0].style = "border-color: red";
+            usernamePSignIn.style = "color: red; opacity: 1";
+        } else {
+            formSignIn[0].style = "border-color: green";
+            usernamePSignIn.style = "opacity: 0";
+        }
+        if (formSignIn[1].value.trim() === "") {
+            isValid = false;
+            formSignIn[1].style = "border-color: red";
+            passworldPSignIn.style = "color: red; opacity: 1";
+        } else {
+            formSignIn[1].style = "border-color: green";
+            passworldPSignIn.style = "opacity: 0";
+        }
+        if (isValid) {
+            setTimeout(function () {
+                alert("Đăng nhập thành công.");
+                formSignIn[0].value = "";
+                formSignIn[1].value = "";
+                formSignIn[0].style = "";
+                formSignIn[1].style = "";
+            }, 500);
+        }
+    });
 });
