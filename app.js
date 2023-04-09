50b441d7cfff16b1f2a6778c16f3243487a2f098
levanhieusona <levanhieusona@gmail.com>
2023-04-09 16:51:58 +0700

    Finish

--- app.js
+++ app.js
@@ -36,9 +36,9 @@ window.addEventListener("load", () => {
     const formSignIn = document.querySelector(".form-sign-in");
     const usernamePSignIn = document.querySelector(".username-sign-in");
     const passworldPSignIn = document.querySelector(".passworld-sign-in");
+    let isValid = true;
     formSignIn.addEventListener("submit", function (e) {
         e.preventDefault();
-        let isValid = true;
         if (formSignIn[0].value.trim() === "") {
             isValid = false;
             formSignIn[0].style = "border-color: red";
@@ -62,6 +62,113 @@ window.addEventListener("load", () => {
                 formSignIn[1].value = "";
                 formSignIn[0].style = "";
                 formSignIn[1].style = "";
+            }, 500);
+        }
+    });
+    const formSignUp = document.querySelector(".form-sign-up");
+    formSignUp.addEventListener("submit", function (e) {
+        e.preventDefault();
+        if (formSignUp.elements[0].value.trim() === "") {
+            formSignUp.children[1].style = "opacity: 1; color: red";
+            formSignUp.elements[0].style = "border-color: red";
+            isValid = false;
+        } else if (formSignUp.elements[0].value.length <= 1) {
+            isValid = false;
+            formSignUp.children[1].style = "opacity: 1; color: red";
+            formSignUp.children[1].textContent =
+                "Vui lòng nhập họ tên phải lớn hơn một kí tự.";
+        } else {
+            formSignUp.children[1].style = "opacity: 1;color: green";
+            formSignUp.elements[0].style = "border-color: green";
+            formSignUp.children[1].textContent = "Họ tên hợp lệ.";
+            isValid = true;
+        }
+
+        // Email
+        let regexEmail =
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (regexEmail.test(formSignUp.elements[1].value.trim())) {
+            formSignUp.children[3].style = "opacity: 1;color: green";
+            formSignUp.elements[1].style = "border-color: green";
+            formSignUp.children[3].textContent = "Email hợp lệ.";
+            isValid = true;
+        } else if (formSignUp.elements[1].value.trim() === "") {
+            formSignUp.children[3].style = "opacity: 1;color: red";
+            formSignUp.elements[1].style = "border-color: red";
+            formSignUp.children[3].textContent = "Email không được để trống.";
+            isValid = false;
+        } else {
+            formSignUp.children[3].style = "opacity: 1;color: red";
+            formSignUp.elements[1].style = "border-color: red";
+            formSignUp.children[3].textContent = "Email không hợp lệ.";
+            isValid = false;
+        }
+
+        // Phone
+        const regexPhoneNumber = /(84|0[3|5|7|8|9])+([0-9]{8})\b/g;
+        if (regexPhoneNumber.test(formSignUp.elements[2].value.trim())) {
+            formSignUp.children[5].style = "opacity: 1;color: green";
+            formSignUp.elements[2].style = "border-color: green";
+            formSignUp.children[5].textContent = "Số điện thoại hợp lệ.";
+            isValid = true;
+        } else if (formSignUp.elements[2].value.trim() === "") {
+            formSignUp.children[5].style = "opacity: 1;color: red";
+            formSignUp.elements[2].style = "border-color: red";
+            formSignUp.children[5].textContent =
+                "Số điện thoại không được để trống.";
+            isValid = false;
+        } else {
+            formSignUp.children[5].style = "opacity: 1;color: red";
+            formSignUp.elements[2].style = "border-color: red";
+            formSignUp.children[5].textContent = "Số điện thoại không hợp lệ.";
+            isValid = false;
+        }
+
+        // Pass
+        if (formSignUp.elements[3].value.trim() === "") {
+            formSignUp.children[7].style = "opacity: 1;color: red";
+            formSignUp.elements[3].style = "border-color: red";
+            formSignUp.children[7].textContent =
+                "Mật khẩu không được để trống.";
+            isValid = false;
+        } else if (formSignUp.elements[3].value.trim().length < 8) {
+            formSignUp.children[7].style = "opacity: 1;color: red";
+            formSignUp.elements[3].style = "border-color: red";
+            formSignUp.children[7].textContent =
+                "Mật khẩu tối đa phải 8 kí tự.";
+            isValid = false;
+        } else {
+            formSignUp.children[7].style = "opacity: 1;color: green";
+            formSignUp.elements[3].style = "border-color: green";
+            formSignUp.children[7].textContent = "Mật khẩu hợp lệ.";
+            isValid = true;
+        }
+
+        // Repeat Pass
+        if (formSignUp.elements[4].value.trim() === "") {
+            formSignUp.children[9].style = "opacity: 1;color: red";
+            formSignUp.elements[4].style = "border-color: red";
+            formSignUp.children[9].textContent =
+                "Nhập lại mật khẩu không được để trống.";
+            isValid = false;
+        } else if (
+            formSignUp.elements[4].value.trim() !==
+            formSignUp.elements[3].value.trim()
+        ) {
+            formSignUp.children[9].style = "opacity: 1;color: red";
+            formSignUp.elements[4].style = "border-color: red";
+            formSignUp.children[9].textContent =
+                "Mật khẩu bạn nhập lại không khớp.";
+            isValid = false;
+        } else {
+            formSignUp.children[9].style = "opacity: 1;color: green";
+            formSignUp.elements[4].style = "border-color: green";
+            formSignUp.children[9].textContent = "Mật khẩu nhập lại hợp lệ.";
+            isValid = true;
+        }
+        if (isValid) {
+            setTimeout(function () {
+                alert("Đăng ký thành công.");
             }, 500);
         }
     });
